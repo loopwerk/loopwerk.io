fb0708f786fc9d2e58703246495e60e1cbec7cb7
Kevin Renskers <kevin@loopwerk.io>
2021-01-20 21:16:56 +0100

    Smaller images

--- ImageWriter/__init__.py
+++ ImageWriter/__init__.py
@@ -13,8 +13,8 @@ class ImageWriter(BaseWriter):
         self._image = Image.open('ImageWriter/background.png')
 
     def start(self):
-        font_small = ImageFont.truetype('ImageWriter/Roboto-Regular.ttf', 40, encoding='unic')
-        font_big = ImageFont.truetype('ImageWriter/Roboto-Regular.ttf', 80, encoding='unic')
+        font_small = ImageFont.truetype('ImageWriter/Roboto-Regular.ttf', 25, encoding='unic')
+        font_big = ImageFont.truetype('ImageWriter/Roboto-Regular.ttf', 55, encoding='unic')
 
         for post in g.public_posts:
             dest = os.path.join(g.output_directory, 'static', 'images', post.filename + '.png')
@@ -23,11 +23,11 @@ class ImageWriter(BaseWriter):
             image = self._image.copy()
 
             draw = ImageDraw.Draw(image)
-            draw.text((50, 35), post.date.strftime('%B %d, %Y'), font=font_small, fill="#FFFFFF")
+            draw.text((30, 15), post.date.strftime('%B %d, %Y'), font=font_small, fill="#FFFFFF")
 
-            offset = 100
-            for line in textwrap.wrap(title, width=40):
-                draw.text((50, offset), line, font=font_big, fill="#FFFFFF")
-                offset += 90
+            offset = 60
+            for line in textwrap.wrap(title, width=36):
+                draw.text((30, offset), line, font=font_big, fill="#FFFFFF")
+                offset += 70
 
             image.save(dest, format="PNG") 
