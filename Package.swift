86271a1c3bd1da6c7695d0af96af55756a86f90e
Kevin Renskers <kevin@loopwerk.io>
2021-02-07 23:07:17 +0100

    Done, back to prod build of Saga (v 0.11.0)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "Loopwerk",
   dependencies: [
-    .package(name: "Saga", url: "https://github.com/loopwerk/Saga.git", .branch("test")),
+    .package(name: "Saga", url: "https://github.com/loopwerk/Saga.git", from: "0.11.0"),
   ],
   targets: [
     .target(
