e190a708c4cdbea2360b6b1ed6a3133c4122d6c1
Kevin Renskers <kevin@loopwerk.io>
2021-02-18 10:49:07 +0100

    Update deps [skip ci]

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .macOS(.v10_15)
   ],
   dependencies: [
-    .package(url: "https://github.com/loopwerk/Saga.git", from: "0.18.4"),
+    .package(url: "https://github.com/loopwerk/Saga.git", from: "0.19.0"),
     .package(url: "https://github.com/loopwerk/SagaParsleyMarkdownReader", from: "0.4.0"),
     .package(url: "https://github.com/loopwerk/SagaSwimRenderer", from: "0.4.0"),
     .package(url: "https://github.com/pvieito/PythonKit.git", from: "0.1.0"),
