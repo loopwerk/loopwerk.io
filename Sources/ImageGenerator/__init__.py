285011f46fb6de75b71c0c64eb6fcc00210984a5
Kevin Renskers <kevin@loopwerk.io>
2022-09-13 16:46:34 +0200

    Nicer article images

--- Sources/ImageGenerator/__init__.py
+++ Sources/ImageGenerator/__init__.py
@@ -6,16 +6,16 @@ from PIL import Image, ImageDraw, ImageFont
 class ImageGenerator:
     def __init__(self, path):
         self._image = Image.open(path+'/ImageGenerator/background.png')
-        self.font_big = ImageFont.truetype(path+'/ImageGenerator/Roboto-Regular.ttf', 55, encoding='unic')
+        self.font_big = ImageFont.truetype(path+'/ImageGenerator/Roboto-Regular.ttf', 60, encoding='unic')
 
     def generate(self, title, date, output_path):
         image = self._image.copy()
 
         draw = ImageDraw.Draw(image)
 
-        offset = 130
-        for line in textwrap.wrap(title, width=19):
-            draw.text((460, offset), line, font=self.font_big, fill="#FFFFFF")
+        offset = 80
+        for line in textwrap.wrap(title, width=32):
+            draw.text((30, offset), line, font=self.font_big, fill="#000000")
             offset += 70
 
         image.save(output_path, format="PNG")
