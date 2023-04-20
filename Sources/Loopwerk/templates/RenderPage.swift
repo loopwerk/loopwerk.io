dad8ca4ce466c753724a7756c00b151a15da7702
Kevin Renskers <kevin@loopwerk.io>
2023-04-20 18:54:45 +0200

    Add Sound Radix

--- Sources/Loopwerk/templates/RenderPage.swift
+++ Sources/Loopwerk/templates/RenderPage.swift
@@ -46,14 +46,15 @@ create a<br>
     }
 
     div(class: "footer") {
-      a(href: "https://www.unilever.nl", title: "Unilever") { img(alt: "Unilever logo", src: "/static/images/unilever.svg") }
-      a(href: "https://www.getsling.com", title: "Sling") { img(alt: "Sling logo", src: "/static/images/sling.svg") }
-      a(href: "https://www.siminn.is", title: "Siminn") { img(alt: "Siminn logo", src: "/static/images/siminn.svg") }
-      a(href: "https://www.last.fm", title: "Last.fm") { img(alt: "Last.fm logo", src: "/static/images/lastfm.svg") }
-      a(href: "https://www.cbs.com", title: "CBS") { img(alt: "CBS logo", src: "/static/images/cbs.svg") }
-      a(href: "https://www.metrolyrics.com", title: "Metrolyrics") { img(alt: "Metrolyrics logo", src: "/static/images/metrolyrics.svg") }
+      a(href: "https://www.soundradix.com", title: "Sound Radix") { img(alt: "Sound Radix logo", src: "/static/images/soundradix.svg") }
       a(href: "https://www.wetransfer.com", title: "WeTransfer") { img(alt: "WeTransfer logo", src: "/static/images/we.svg") }
       a(href: "https://www.sentry.io", title: "Sentry") { img(alt: "Sentry logo", src: "/static/images/sentry.svg") }
+      a(href: "https://www.unilever.nl", title: "Unilever") { img(alt: "Unilever logo", src: "/static/images/unilever.svg") }
+      a(href: "https://www.last.fm", title: "Last.fm") { img(alt: "Last.fm logo", src: "/static/images/lastfm.svg") }
+      a(href: "https://www.getsling.com", title: "Sling") { img(alt: "Sling logo", src: "/static/images/sling.svg") }
+      a(href: "https://www.siminn.is", title: "Siminn") { img(alt: "Siminn logo", src: "/static/images/siminn.svg") }
+      a(href: "https://www.cbs.com", title: "CBS") { img(alt: "CBS logo", src: "/static/images/cbs.svg") }
+      a(href: "https://www.metrolyrics.com", title: "Metrolyrics") { img(alt: "Metrolyrics logo", src: "/static/images/metrolyrics.svg") }
       a(class: "hidden", href: "https://fosstodon.org/@kevinrenskers", rel: "me") { "@kevinrenskers@fosstodon.org" }
     }
   }
---
+++ content/apps/soundradix.md
@@ -0,0 +1,11 @@
+---
+images: sr_1.png, sr_2.png
+roundOffImages: false
+breakImages: 1
+url: https://www.soundradix.com
+date: 2023-01-01
+---
+
+# Sound Radix
+
+Sound Radix is an Emmy award winning software studio building innovative software tools to help you achieve the best sound possible. I took over development and maintenance of their SvelteKit and Django powered website and webshop.
