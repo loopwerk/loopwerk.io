0ca6f1ecf7361af91f2b3e50940f39893c6caa37
Kevin Renskers <kevin@loopwerk.io>
2024-09-26 14:21:27 +0200

    Comments via giscus (#14)

--- Sources/Loopwerk/templates/RenderArticle.swift
+++ Sources/Loopwerk/templates/RenderArticle.swift
@@ -66,6 +66,25 @@ func renderArticle(context: ItemRenderingContext<ArticleMetadata>) -> Node {
 
       div(class: "article_content") {
         Node.raw(context.item.body)
+        
+        Node.raw("""
+<script src="https://giscus.app/client.js"
+        data-repo="loopwerk/loopwerk.io"
+        data-repo-id="MDEwOlJlcG9zaXRvcnk0Nzg0NTA3MA=="
+        data-category="Article discussions"
+        data-category-id="DIC_kwDOAtoOzs4Ciykw"
+        data-mapping="pathname"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
+""")
       }
 
       ul(class: "pagination") {
