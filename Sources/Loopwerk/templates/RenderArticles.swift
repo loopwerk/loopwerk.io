f76dfefd8573f38da28777b220f1c8365c35c7da
Kevin Renskers <kevin@loopwerk.io>
2025-07-03 14:11:28 +0200

    Add tag cloud to articles page

--- Sources/Loopwerk/templates/RenderArticles.swift
+++ Sources/Loopwerk/templates/RenderArticles.swift
@@ -37,8 +37,24 @@ func renderArticles(context: ItemsRenderingContext<ArticleMetadata>) -> Node {
 
   let articlesPerYear = Dictionary(grouping: context.items, by: { dateFormatter.string(from: $0.date) })
   let sortedByYearDescending = articlesPerYear.sorted { $0.key > $1.key }
+  
+  let tagsWithCounts = uniqueTagsWithCount(context.items)
 
   return baseLayout(canocicalURL: "/articles/", section: .articles, title: "Articles", rssLink: "") {
+    // Tag cloud section
+    div(class: "mb-12") {
+      h2(class: "text-lg font-bold") { "Browse by tag" }
+      
+      div(class: "flex flex-wrap gap-x-2 text-gray gray-links text-sm") {
+        tagsWithCounts.map { tag, count in
+          a(href: "/articles/tag/\(tag.slugified)/") {
+            "\(tag) (\(count))"
+          }
+        }
+      }
+    }
+    
+    // Articles by year
     sortedByYearDescending.map { year, articles in
       div {
         h1(class: "text-4xl font-extrabold mb-12") { year }
@@ -52,17 +68,12 @@ func renderArticles(context: ItemsRenderingContext<ArticleMetadata>) -> Node {
 }
 
 func _renderArticles(_ articles: [Item<ArticleMetadata>], canocicalURL: String, title pageTitle: String, rssLink: String = "", extraHeader: NodeConvertible = Node.fragment([])) -> Node {
-  return baseLayout(canocicalURL: canocicalURL, section: .articles, title: pageTitle, rssLink: rssLink, extraHeader: extraHeader) {
-    articles.map { article in
-      section(class: "mb-10") {
-        h1(class: "text-2xl font-bold mb-2") {
-          a(class: "[&:hover]:border-b border-orange", href: article.url) { article.title }
-        }
-        renderArticleInfo(article)
-        p(class: "mt-4") {
-          a(href: article.url) { article.summary }
-        }
-      }
+  return baseLayout(canocicalURL: canocicalURL, section: .articles, title: "articles in \(pageTitle)", rssLink: rssLink, extraHeader: extraHeader) {
+    
+    h1(class: "text-4xl font-extrabold mb-12") { pageTitle }
+    
+    div(class: "grid lg:grid-cols-2 gap-10 mb-16") {
+      articles.map { renderArticleForGrid(article: $0) }
     }
   }
 }
@@ -70,9 +81,9 @@ func _renderArticles(_ articles: [Item<ArticleMetadata>], canocicalURL: String, 
 func renderTag<T>(context: PartitionedRenderingContext<T, ArticleMetadata>) -> Node {
   let extraHeader = link(href: "/articles/tag/\(context.key.slugified)/feed.xml", rel: "alternate", title: "\(SiteMetadata.name): articles with tag \(context.key)", type: "application/rss+xml")
 
-  return _renderArticles(context.items, canocicalURL: "/articles/tag/\(context.key.slugified)/", title: "Articles in \(context.key)", rssLink: "tag/\(context.key.slugified)/", extraHeader: extraHeader)
+  return _renderArticles(context.items, canocicalURL: "/articles/tag/\(context.key.slugified)/", title: "#\(context.key)", rssLink: "tag/\(context.key.slugified)/", extraHeader: extraHeader)
 }
 
 func renderYear<T>(context: PartitionedRenderingContext<T, ArticleMetadata>) -> Node {
-  _renderArticles(context.items, canocicalURL: "/articles/\(context.key)/", title: "Articles in \(context.key)")
+  _renderArticles(context.items, canocicalURL: "/articles/\(context.key)/", title: "\(context.key)")
 }
