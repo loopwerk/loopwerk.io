644c3b54001f579a562b42ab09f367b2c3c10de8
Kevin Renskers <kevin@loopwerk.io>
2026-02-22 10:27:16 +0100

    Give prism.css also a unique hash

--- hash-css.sh
+++ hash-css.sh
@@ -3,35 +3,38 @@ set -e
 
 # Build directory where CSS and HTML files are located
 BUILD_DIR="deploy"
-CSS_FILE="$BUILD_DIR/static/output.css"
+CSS_FILES=("output.css" "prism.css")
 
-# Check if CSS file exists
-if [ ! -f "$CSS_FILE" ]; then
-    echo "Error: CSS file not found at $CSS_FILE"
-    exit 1
-fi
+for CSS_NAME in "${CSS_FILES[@]}"; do
+    CSS_FILE="$BUILD_DIR/static/$CSS_NAME"
 
-# Generate MD5 hash of the CSS file
-CSS_HASH=$(md5sum "$CSS_FILE" | cut -d' ' -f1 | cut -c1-8)
-
-# Create new filename with hash
-CSS_NEW_NAME="output-${CSS_HASH}.css"
-CSS_NEW_PATH="$BUILD_DIR/static/$CSS_NEW_NAME"
-
-# Rename the CSS file
-mv "$CSS_FILE" "$CSS_NEW_PATH"
-echo "Renamed CSS file to: $CSS_NEW_NAME"
-
-# Update all HTML files to reference the new CSS filename
-find "$BUILD_DIR" -name "*.html" -type f | while read -r html_file; do
-    # Replace references to output.css with the hashed version
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS
-        sed -i '' "s|/static/output\.css|/static/${CSS_NEW_NAME}|g" "$html_file"
-    else
-        # Linux/Debian
-        sed -i "s|/static/output\.css|/static/${CSS_NEW_NAME}|g" "$html_file"
+    # Check if CSS file exists
+    if [ ! -f "$CSS_FILE" ]; then
+        echo "Error: CSS file not found at $CSS_FILE"
+        exit 1
     fi
-done
 
-echo "Updated all HTML files to reference: $CSS_NEW_NAME"
+    # Generate MD5 hash of the CSS file
+    CSS_HASH=$(md5sum "$CSS_FILE" | cut -d' ' -f1 | cut -c1-8)
+
+    # Create new filename with hash
+    CSS_BASE="${CSS_NAME%.css}"
+    CSS_NEW_NAME="${CSS_BASE}-${CSS_HASH}.css"
+
+    # Rename the CSS file
+    mv "$CSS_FILE" "$BUILD_DIR/static/$CSS_NEW_NAME"
+    echo "Renamed $CSS_NAME to: $CSS_NEW_NAME"
+
+    # Update all HTML files to reference the new CSS filename
+    find "$BUILD_DIR" -name "*.html" -type f | while read -r html_file; do
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            # macOS
+            sed -i '' "s|/static/${CSS_NAME}|/static/${CSS_NEW_NAME}|g" "$html_file"
+        else
+            # Linux/Debian
+            sed -i "s|/static/${CSS_NAME}|/static/${CSS_NEW_NAME}|g" "$html_file"
+        fi
+    done
+
+    echo "Updated all HTML files to reference: $CSS_NEW_NAME"
+done
